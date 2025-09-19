cfc9e3316d95f85bfb5103b7fb0e057933ce785a
rsoleyma <ramin.soleymani@bsc.es>
2025-09-19 16:39:07 +0200

    humanize cmd duration

--- tools/typer_log.py
+++ tools/typer_log.py
@@ -1,29 +1,47 @@
 import json
 import os
+from datetime import datetime
+from pathlib import Path
+from typing import Optional
 
+import humanize
 import typer
 
 from tools.env_root import root
 
 
-def patch_typer_invoke():
-    file = root() / "data/typer-log.json"
-    file.parent.mkdir(parents=True, exist_ok=True)
+def patch_typer_invoke(log_fp: Optional[Path] = None):
+    if not log_fp:
+        log_fp = root() / "data/typer-log.jsonl"
+    elif log_fp.suffix != ".jsonl":
+        print("typer-log should be a jsonl file")
+    log_fp.parent.mkdir(parents=True, exist_ok=True)
 
     # Store the original invoke method
     _original_invoke = typer.core.TyperCommand.invoke
 
     def patched_invoke(self, ctx):
         # This runs after parsing but before command execution
-        with file.open("a", encoding="utf-8") as fout:
-            fout.write(json.dumps(
-                {"cmd": ctx.command.name, "params": ctx.params},
-            ) + os.linesep)
+
+        start = datetime.now()
+        row = {"cmd": ctx.command.name,
+             "params": ctx.params,
+             "ts": start.isoformat(timespec="minutes")}
+
         # print(f"Command: {ctx.command.name}")
         # print(f"Params: {ctx.params}")  # Will have all your arguments!
 
         # Call the original command
-        return _original_invoke(self, ctx)
+        try:
+            res = _original_invoke(self, ctx)
+            print(res)
+        except Exception as e:
+            row["error"] = str(e)
+        finally:
+            row["duration"] = humanize.naturaldelta(datetime.now() - start)
+            with log_fp.open("a", encoding="utf-8") as fout:
+                fout.write(json.dumps(row) + os.linesep)
+            return res
 
     # Apply the patch
     typer.core.TyperCommand.invoke = patched_invoke
